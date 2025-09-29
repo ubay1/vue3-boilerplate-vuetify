f46b7e66d2c44ca24e44c4decaefc3d992c6950c
ubay1 <tomdelonge976@gmail.com>
2025-09-29 09:48:32 +0700

    fix vitest

--- src/__tests__/unit/setup.ts
+++ src/__tests__/unit/setup.ts
@@ -1,5 +1,23 @@
 // fixing error window.matchMedia is not a function
-import { vi } from 'vitest'
+import { beforeAll, vi } from 'vitest'
+
+// fix error ReferenceError: visualViewport is not defined
+beforeAll(() => {
+  Object.defineProperty(window, "visualViewport", {
+    value: {
+      width: 1024,
+      height: 768,
+      scale: 1,
+      offsetTop: 0,
+      offsetLeft: 0,
+      pageTop: 0,
+      pageLeft: 0,
+      addEventListener: () => { },
+      removeEventListener: () => { },
+    },
+    writable: true,
+  })
+})
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
